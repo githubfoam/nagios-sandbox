674d88c61ce20b63d197e0bacdeaa1b23f748712
Your Name <you@example.com>
2021-04-05 11:43:19 +0300

    abeille

---
+++ scripts/deploy-nagios-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+# set -eox pipefail #safety for script
+
+echo "============================deploy nagios docker image============================================================="
+
+cd dockerfiles 
+docker build . --file Dockerfile.ubuntu2004 --tag $IMAGE_NAME
+docker image ls
+
+docker run -d                            \
+-p 80:80 -p 443:443 -p 5666:5666         \
+$IMAGE_NAME
+docker container ls
+
+echo "============================deploy nagios docker image============================================================="
