eac7b089877583919ad1bb8847dc633d89eb1f86
Your Name <you@example.com>
2021-04-05 12:48:53 +0300

    indispensables

--- scripts/deploy-libvirt.sh
+++ scripts/deploy-libvirt.sh
@@ -13,7 +13,8 @@ libvirtd --version
 #If 0 it means that your CPU doesn't support hardware virtualization.
 # If 1 or more it does - but you still need to make sure that virtualization is enabled in the BIOS.
 # egrep -c '(vmx|svm)' /proc/cpuinfo #0
+egrep -c '(vmx|svm)' /proc/cpuinfo | echo "virtualization is  supported" | echo "virtualization is not supported"
 addgroup libvirtd
 adduser  $(id -un) libvirtd #ensure that your username is added to the group libvirtd
-kvm-ok
+# kvm-ok #INFO: Your CPU does not support KVM extensions
 echo "=============================Install kvm qemu libvirt finished============================================================="
